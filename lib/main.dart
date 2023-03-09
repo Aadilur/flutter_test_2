00f3cf92c0654fac68fb126078cecaf059d04ee9
Aadilur <hardnut103@gmail.com>
2023-03-10 00:18:11 +0600

    day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test_2/pages/home_page.dart';
 import 'package:flutter_test_2/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
       theme: ThemeData(
-        primarySwatch: Colors.green
+        primarySwatch: Colors.green,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        primaryTextTheme: GoogleFonts.latoTextTheme(),
       ),
       darkTheme: ThemeData(
         primarySwatch: Colors.deepOrange,
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -6,11 +8,73 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login Page",style: TextStyle(
-          fontSize: 20,
-          color: Colors.blueGrey,
-        ),),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/login.png",
+            fit: BoxFit.cover,
+          ),
+
+          // SvgPicture.asset(
+          //   "assets/svg/login.svg",
+          //   width: 360,
+          // ),
+
+          SizedBox(height: 10,),
+
+          Text(
+            "Welcome",
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+                letterSpacing: 4,
+                color: Colors.black45),
+          ),
+
+          SizedBox(height: 20,),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50,vertical: 20),
+            child: Column(children: [
+
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Enter User Name",
+                  labelText: "User Name",
+                ),
+              ),
+
+              SizedBox(height: 20,),
+
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Enter Password",
+                  labelText: "Password",
+                ),
+              )
+
+            ],),
+          ),
+
+
+          SizedBox(height: 20,),
+          
+          ElevatedButton(onPressed:()=>{debugPrint("-0--------------------------------")}, child: Text("Login")),
+
+
+          // QrImage(
+          //   data: "REX Antor is a Computer Engineer",
+          //   size: 350,
+          //   gapless: false,
+          //   version: QrVersions.auto,
+          //   backgroundColor: Colors.transparent,
+          //   dataModuleStyle: const QrDataModuleStyle(
+          //       dataModuleShape: QrDataModuleShape.circle, color: Colors.green),
+          //   eyeStyle: const QrEyeStyle(
+          //       eyeShape: QrEyeShape.square, color: Colors.green),
+          // )
+        ],
       ),
     );
   }
